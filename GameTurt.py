7f1979d23970e4f3c1d3ef8753a593b10be5eea7
abdouna <abdouna@berea.edu>
2026-04-17 01:23:49 -0400

    created 7 scrambling turtles, still figuring out collision problem

--- GameTurt.py
+++ GameTurt.py
@@ -1,6 +1,7 @@
 import turtle
 import turtle as tr
 import time
+import random
 
 class GameTurt():
     def __init__(self):
@@ -44,13 +45,56 @@ class GameTurt():
         d.goto(252,230)
         d.write("Level 1", align= 'center', font=('Arial',30,'normal'))
 
-class RunningTurt():
+class EnemyTurt():
     def __init__(self,color):
-        self.turtle = tr.Turtle
+        self.turtle = tr.Turtle()
         self.alive = True
         self.shape = self.turtle.shape("turtle")
         self.color = color
         self.turtle.color(color)
+        self.turtle.penup()
+        self.move_counter = 0 # this is here to count frames in move()
+
+    def move(self):
+        self.move_counter += 1
+        if self.move_counter % 7 == 0:  # only move every 7 frames for each turtle
+            self.turtle.left(random.randint(30, 280))
+            self.bounce()
+            self.turtle.forward(random.randint(20,100))
 
 
-screen = GameTurt()
+    def bounce(self):
+        """
+        this method ensures that none of the turtles will go off screen.
+        if the turtle leaves the bounds it will be moved back to the edge of the
+        screen
+        """
+
+        x = self.turtle.xcor()
+        y = self.turtle.ycor()
+
+        if x > 300:
+            self.turtle.setx(300)
+        if x < -300:
+            self.turtle.setx(-300)
+        if y > 200:
+            self.turtle.sety(200)
+        if y < -200:
+            self.turtle.sety(-200)
+
+
+
+
+def main():
+    tr.tracer(2) #only updates screen after every turtle has moved once
+    colors = ["blue","purple","red","orange","yellow","green","black"]
+    enemies = []
+    for color in colors:
+        e = EnemyTurt(color)
+        enemies.append(e)
+    while True:
+        for e in enemies:
+            e.move()
+        tr.update()
+        time.sleep(0.054)
+main()
